f676a729c47d40556b6f771527926da2dea4616f
jamesabrannan@hushmail.com <jamesabrannan@hushmail.com>
2026-05-18 11:49:07 -0400

    fixes to parallel example for blog

---
+++ html-converter-dotnet-core-cs/ConverterParallelExample.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace html_converter_dotnet_core_cs
+{
+    internal class ConverterParallelExample
+    {
+    }
+}
--- html-converter-dotnet-core-cs/Program.cs
+++ html-converter-dotnet-core-cs/Program.cs
@@ -9,54 +9,54 @@ namespace html_converter_dotnet_core_cs
          {
             Util.CreatePath("Output");
 
-            //   SimpleConversion.Run(Util.GetPath("Output/SimpleConversion.pdf"));
-            //   Console.WriteLine("SimpleConversion is completed...");
+            SimpleConversion.Run(Util.GetPath("Output/SimpleConversion.pdf"));
+            Console.WriteLine("SimpleConversion is completed...");
 
-            //    string inputPath = Util.GetPath("../html-converter-dotnet-core-cs/Resources/products.html");
-            //    SimpleConversion2.Run(inputPath, Util.GetPath("Output/"));
-            //    Console.WriteLine("SimpleConversion2 is completed...");
+            string inputPath = Util.GetPath("../html-converter-dotnet-core-cs/Resources/products.html");
+            SimpleConversion2.Run(inputPath, Util.GetPath("Output/"));
+            Console.WriteLine("SimpleConversion2 is completed...");
 
-            //      EventHandlingExample.Run(Util.GetPath("Output/EventHandling.pdf"));
-            //     Console.WriteLine("EventHandlingExample is completed...");
+            EventHandlingExample.Run(Util.GetPath("Output/EventHandling.pdf"));
+            Console.WriteLine("EventHandlingExample is completed...");
 
             CancelationTokenExample.RunAsync().GetAwaiter().GetResult();
             Console.WriteLine("CancelationTokenExample is completed...");
 
-            /*
-                        HtmlConversionUsingString.Run();
-                        Console.WriteLine("HtmlConversionUsingString is completed...");
 
-                        ConvertToByteArray.Run();
-                        Console.WriteLine("ConvertToByteArray is completed...");
+            HtmlConversionUsingString.Run();
+            Console.WriteLine("HtmlConversionUsingString is completed...");
 
-                        JavaScriptCssConversion.Run();
-                        Console.WriteLine("JavaScriptConversion is completed...");
+            ConvertToByteArray.Run();
+            Console.WriteLine("ConvertToByteArray is completed...");
 
-                        CssFileConversion.Run();
-                        Console.WriteLine("CssFileConversion is completed...");
+            JavaScriptCssConversion.Run();
+            Console.WriteLine("JavaScriptConversion is completed...");
 
-                        ImageLocalExample.Run();
-                        Console.WriteLine("ImageLocalExample is completed...");
+            CssFileConversion.Run();
+            Console.WriteLine("CssFileConversion is completed...");
 
-                        AsyncConversion.Run();
-                        Console.WriteLine("AsyncConversion is completed...");
+            ImageLocalExample.Run();
+            Console.WriteLine("ImageLocalExample is completed...");
 
-                        BasePathExample.Run();
-                        Console.WriteLine("BasePathExample conversion is completed...");
+            AsyncConversion.Run();
+            Console.WriteLine("AsyncConversion is completed...");
 
-                        FileConversion.Run();
-                        Console.WriteLine("Conversion with file content and anchor tag completed...");
+            BasePathExample.Run();
+            Console.WriteLine("BasePathExample conversion is completed...");
+
+            FileConversion.Run();
+            Console.WriteLine("Conversion with file content and anchor tag completed...");
 
 
-                        WithConversionOptions.Run();
-                        Console.WriteLine("WithConversionOptions is completed...");
-                        */
+            WithConversionOptions.Run();
+            Console.WriteLine("WithConversionOptions is completed...");
 
-            // TimeoutExceptionsExample.Run();
-            //    Console.WriteLine("TimeoutExampleConversion is completed...");
 
-            // ConverterParallelExample.Run().Wait();
-            //  Console.WriteLine("ConverterParallelExample is completed...");
+            TimeoutExceptionsExample.Run();
+            Console.WriteLine("TimeoutExampleConversion is completed...");
+
+            ConverterParallelExample.Run().Wait();
+            Console.WriteLine("ConverterParallelExample is completed...");
 
             Console.WriteLine("Completed...");
 
